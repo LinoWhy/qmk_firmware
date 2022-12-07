e37bfb4b75509854de4dfc3f676f83b2535cdf63
YUAN Lijun <yuan.lijun@hotmail.com>
2022-12-07 09:17:35 +0800

    switch lgui & lshift
    Signed-off-by: YUAN Lijun <yuan.lijun@hotmail.com>

--- keyboards/lily58/keymaps/default/keymap.c
+++ keyboards/lily58/keymaps/default/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * | LCTRL|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RSHIFT|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |LSHIFT| LGUI | LAlt | / LOWER /       \Space \  |  -   |  =   |  '   |
+ *                   | LGUI |LSHIFT| LAlt | / LOWER /       \Space \  |  -   |  =   |  '   |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   KC_GRV,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
   KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                        KC_LSFT, KC_LGUI, KC_LALT, MO(_LOWER), KC_SPC, KC_MINS,    KC_EQL,  KC_QUOT
+                        KC_LGUI, KC_LSFT, KC_LALT, MO(_LOWER), KC_SPC, KC_MINS,    KC_EQL,  KC_QUOT
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   (   |    |    )  |------+------+------+------+------+------|
  * | LCTRL|      |      |      |      |      |-------|    |-------|  INS | DEL  |      |      |      |RSHIFT|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | CAPS | LGUI | LAlt | / LOWER /       \Space \  |  -   |  =   |  '   |
+ *                   | LGUI | CAPS | LAlt | / LOWER /       \Space \  |  -   |  =   |  '   |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_F11,  KC_F12,                    KC_HOME, KC_PGUP, KC_PGDN, KC_END,   KC_PSCR, _______,
   XXXXXXX, KC_INS,  XXXXXXX, KC_DEL,  XXXXXXX, XXXXXXX,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, XXXXXXX, _______,
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LPRN, KC_RPRN, KC_INS,  KC_DEL,  XXXXXXX, XXXXXXX,  XXXXXXX, _______,
-                             KC_CAPS, _______, _______, _______, _______,  XXXXXXX, XXXXXXX, XXXXXXX
+                             _______, KC_CAPS, _______, _______, _______,  XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
 [_RAISE] = LAYOUT(
