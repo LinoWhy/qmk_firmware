121d27e01e1ecf6c29483fab19db22632236132b
YUAN Lijun <yuan.lijun@hotmail.com>
2022-12-08 13:46:23 +0800

    Revert "backspace"
    This reverts commit f20ee2a9dfb21a8f6502b0f532729d5670603ad0.

--- keyboards/lily58/keymaps/default/keymap.c
+++ keyboards/lily58/keymaps/default/keymap.c
@@ -11,12 +11,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  ]   |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  <-  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |  Tab |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  \   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |  ESC |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  | ENT  |
- * |------+------+------+------+------+------|   [   |    |   <-  |------+------+------+------+------+------|
+ * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |LSHIFT|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RSHIFT|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI | LCTRL| / LOWER /       \Space \  |  -   |  =   |  '   |
@@ -25,20 +25,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_QWERTY] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_RBRC,
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_BSPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                         KC_LALT, KC_LGUI, KC_LCTL, MO(_LOWER), KC_SPC, KC_MINS,    KC_EQL,  KC_QUOT
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |   `  |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  | F9   | F10  |  )   |
+ * |   `  |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  | F9   | F10  |  <-  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |  Tab |      |      |      |  F11 |  F12 |                    | HOME | PGUP | PGDN | END  | PRINT|  \   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |  ESC | INS  |      | DEL  |      |      |-------.    ,-------| Left | Down |  Up  | Right|      |  ENT |
- * |------+------+------+------+------+------|   (   |    |  <-   |------+------+------+------+------+------|
+ * |------+------+------+------+------+------|   (   |    |    )  |------+------+------+------+------+------|
  * |LSHIFT|      |      |      |      |      |-------|    |-------|  INS | DEL  |      |      |      | CAPS |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI | LCTRL| / LOWER /       \Space \  |  -   |  =   |  '   |
@@ -46,10 +46,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `----------------------------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT(
-  XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_RPRN,
+  XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  _______,
   _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_F11,  KC_F12,                    KC_HOME, KC_PGUP, KC_PGDN, KC_END,   KC_PSCR, _______,
   _______, KC_INS,  XXXXXXX, KC_DEL,  XXXXXXX, XXXXXXX,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, XXXXXXX, _______,
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LPRN, _______, KC_INS,  KC_DEL,  XXXXXXX, XXXXXXX,  XXXXXXX, KC_CAPS,
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LPRN, KC_RPRN, KC_INS,  KC_DEL,  XXXXXXX, XXXXXXX,  XXXXXXX, KC_CAPS,
                              _______, _______, _______, _______, _______,  XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
