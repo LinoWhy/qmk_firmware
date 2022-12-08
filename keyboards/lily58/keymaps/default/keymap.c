d1522c73414a7417e4a87f904ad6b45deaa5e29a
YUAN Lijun <yuan.lijun@hotmail.com>
2022-12-08 12:26:08 +0800

    ctrl, shift, alt, esc
    Signed-off-by: YUAN Lijun <yuan.lijun@hotmail.com>

--- keyboards/lily58/keymaps/default/keymap.c
+++ keyboards/lily58/keymaps/default/keymap.c
@@ -11,46 +11,46 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  ESC |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  <-  |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  <-  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |  Tab |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  \   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |   `  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  | ENT  |
+ * |  ESC |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  | ENT  |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
- * | LCTRL|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RSHIFT|
+ * |LSHIFT|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RSHIFT|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LGUI |LSHIFT| LAlt | / LOWER /       \Space \  |  -   |  =   |  '   |
+ *                   | LAlt | LGUI | LCTRL| / LOWER /       \Space \  |  -   |  =   |  '   |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
 [_QWERTY] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-  KC_GRV,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-  KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                        KC_LGUI, KC_LSFT, KC_LALT, MO(_LOWER), KC_SPC, KC_MINS,    KC_EQL,  KC_QUOT
+  KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                        KC_LALT, KC_LGUI, KC_LCTL, MO(_LOWER), KC_SPC, KC_MINS,    KC_EQL,  KC_QUOT
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  ESC |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  | F9   | F10  |  <-  |
+ * |   `  |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  | F9   | F10  |  <-  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |  Tab |      |      |      |  F11 |  F12 |                    | HOME | PGUP | PGDN | END  | PRINT|  \   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |   `  | INS  |      | DEL  |      |      |-------.    ,-------| Left | Down |  Up  | Right|      |  ENT |
+ * |  ESC | INS  |      | DEL  |      |      |-------.    ,-------| Left | Down |  Up  | Right|      |  ENT |
  * |------+------+------+------+------+------|   (   |    |    )  |------+------+------+------+------+------|
- * | LCTRL|      |      |      |      |      |-------|    |-------|  INS | DEL  |      |      |      |RSHIFT|
+ * |LSHIFT|      |      |      |      |      |-------|    |-------|  INS | DEL  |      |      |      | CAPS |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LGUI | CAPS | LAlt | / LOWER /       \Space \  |  -   |  =   |  '   |
+ *                   | LAlt | LGUI | LCTRL| / LOWER /       \Space \  |  -   |  =   |  '   |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT(
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  _______,
+  XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  _______,
   _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_F11,  KC_F12,                    KC_HOME, KC_PGUP, KC_PGDN, KC_END,   KC_PSCR, _______,
-  XXXXXXX, KC_INS,  XXXXXXX, KC_DEL,  XXXXXXX, XXXXXXX,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, XXXXXXX, _______,
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LPRN, KC_RPRN, KC_INS,  KC_DEL,  XXXXXXX, XXXXXXX,  XXXXXXX, _______,
-                             _______, KC_CAPS, _______, _______, _______,  XXXXXXX, XXXXXXX, XXXXXXX
+  _______, KC_INS,  XXXXXXX, KC_DEL,  XXXXXXX, XXXXXXX,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, XXXXXXX, _______,
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LPRN, KC_RPRN, KC_INS,  KC_DEL,  XXXXXXX, XXXXXXX,  XXXXXXX, KC_CAPS,
+                             _______, _______, _______, _______, _______,  XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
 [_RAISE] = LAYOUT(
