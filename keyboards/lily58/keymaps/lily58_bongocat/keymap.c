ea07c5ed798835f52815956a932bee9f7b836342
YUAN Lijun <yuan.lijun@hotmail.com>
2023-01-31 16:48:44 +0800

    fix page down & up
    Signed-off-by: YUAN Lijun <yuan.lijun@hotmail.com>

--- keyboards/lily58/keymaps/lily58_bongocat/keymap.c
+++ keyboards/lily58/keymaps/lily58_bongocat/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT(
   XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  _______,
-  _______, KC_QUOT, XXXXXXX, KC_EQL,  KC_F11,  KC_F12,                    KC_HOME, KC_PGUP, KC_PGDN, KC_END,   KC_PSCR, _______,
+  _______, KC_QUOT, XXXXXXX, KC_EQL,  KC_F11,  KC_F12,                    KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_PSCR, _______,
   _______, KC_PPLS, XXXXXXX, KC_DQUO, XXXXXXX, XXXXXXX,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, XXXXXXX, _______,
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LPRN, KC_RPRN, KC_INS,  KC_DEL,  XXXXXXX, XXXXXXX,  XXXXXXX, KC_CAPS,
                              _______, _______, _______, _______, _______,  XXXXXXX, XXXXXXX, XXXXXXX
